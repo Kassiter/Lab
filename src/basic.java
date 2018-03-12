f3ab2f46d8622410a2f885b45185a7e0f31d90ab
T-double-U <trat.westerholt@gmail.com>
2018-03-12 20:36:40 +0200

    2 commit | Added constructor and fields

--- src/basic.java
+++ src/basic.java
@@ -1,4 +1,15 @@
-
+/*
+ * Lab
+ * version 1.1
+ * T-double-U
+ */
 public class basic {
-
+	private String name;
+	private int amount;
+	private double version;
+	public basic(String n, int a, double v) {
+		this.name=n;
+		this.amount = a;
+		this.version=v;
+	}
 }
